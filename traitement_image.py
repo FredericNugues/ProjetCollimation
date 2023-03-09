7fc60fb0462b8073a9588211d0beeee1dc656149
Frederic Nugues <frederic.nugues@imt-atlantique.net>
2023-03-09 10:37:20 +0100

    voici un nouveau test pour la branche main

--- traitement_image.py
+++ traitement_image.py
@@ -1,6 +1,17 @@
 # -*- coding: utf-8 -*-
-import numpy as np
-import pillow as plw
 
-print("Hello tout le monde !")
+import matplotlib.pyplot as plt
+from PIL import Image
+from skimage import io
 
+donut1 = Image.open("D:\IMT_A\projet_Codevsi\donut1.png")
+donut2 = Image.open("D:\IMT_A\projet_Codevsi\donut2.png")
+
+image1 = io.imread("D:\IMT_A\projet_Codevsi\donut1.png")
+#image2 = io.imread("D:\IMT_A\projet_Codevsi\donut2.png")
+plt.imshow(image1) # Pour plot l'image 
+
+
+
+
+#donut1.show() #Pour afficher l'image sur une nouvelle fenêtre
