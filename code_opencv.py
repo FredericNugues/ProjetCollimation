f5cb403ce648a4682655f91980b4659f09983b6e
Frederic Nugues <frederic.nugues@imt-atlantique.net>
2023-03-30 18:32:02 +0200

    Voici le code permettant de trouver le cercle qui approxime le gros disque et le petit

--- code_opencv.py
+++ code_opencv.py
@@ -10,35 +10,23 @@ import numpy as np
 from skimage import measure
 from matplotlib import pyplot as plt
 
-def find_circle(img): # l'image doit déjà avoir été filtré, il doit y avoir directement le disque et non le donut
-    # blur
-    blur = cv.GaussianBlur(img, (3,3), 0)
-    # threshold
-    thresh = cv.threshold(blur, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)[1]
+def detect_contour(image):
+    # Appliquer un flou gaussien pour réduire les bruits de l'image
+    blur = cv.GaussianBlur(image, (5, 5), 0)
 
-    # apply morphology open with a circular shaped kernel
-    kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (5,5))
-    binary = cv.morphologyEx(thresh, cv.MORPH_OPEN, kernel, iterations=2)
+    # Trouver les contours dans l'image binaire
+    contours, hierarchy = cv.findContours(blur, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    # Trouver le contour le plus grand
+    if not contours: 
+        print('pas de cercle')
+        return None
+    max_contour = max(contours, key=cv.contourArea)
 
-    # find contour and draw on input (for comparison with circle)
-    cnts = cv.findContours(binary, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
-    c = cnts[0]
-    result = img.copy()
-    cv.drawContours(result, [c], -1, (0, 255, 0), 1)
+    # Approximer le contour par un cercle en utilisant la méthode des moindres carrés
+    (x, y), radius = cv.minEnclosingCircle(max_contour)
+    center, radius = (int(x),int(y)), int(radius)
+    return center, radius
 
-    # find radius and center of equivalent circle from binary image and draw circle
-    # see https://scikit-image.org/docs/dev/api/skimage.measure.html#skimage.measure.regionprops
-    # Note: this should be the same as getting the centroid and area=cv2.CC_STAT_AREA from cv2.connectedComponentsWithStats and computing radius = 0.5*sqrt(4*area/pi) or approximately from the area of the contour and computed centroid via image moments.
-    regions = measure.regionprops(binary)
-    circle = regions[0]
-    yc, xc = circle.centroid
-    radius = circle.equivalent_diameter / 2.0
-    print("radius =",radius, "  center =",xc,",",yc)
-    xx = int(round(xc))
-    yy = int(round(yc))
-    rr = int(round(radius))
-    return ((xx,yy),rr)
     
 # Charger l'image et la convertir en niveau de gris
 img = cv.imread("D:\IMT_A\projet_Codevsi\donut1.png")
@@ -59,8 +47,8 @@ im_floodfill = th.copy()
 h, w = th.shape[:2] # On récupère les dimensions hauteur et largeur de l'image
 mask = np.zeros((h+2, w+2), np.uint8)
 cv.floodFill(im_floodfill, mask, (0,0), 255) # Le contour à remplir est en noir et le reste en blanc
-im_floodfill_inv = cv.bitwise_not(im_floodfill) # C'est pourquoi nous inversons l'image afin d'avoir la zone à remplir en blanc
-th = th | im_floodfill_inv
+Pdisque = cv.bitwise_not(im_floodfill) # C'est pourquoi nous inversons l'image afin d'avoir la zone à remplir en blanc
+th = th | Pdisque
 
 # Enlèvement des bords de l'image
 Gdisque = th[bordersize:len(th)-bordersize, bordersize:len(th[0])-bordersize]
@@ -73,14 +61,33 @@ resultat = cv.bitwise_and(img_resized, img_resized, mask=Gdisque)
 
 
 
+# Appeler la fonction detect_contour
+if detect_contour(Gdisque) != None:
+    center1, radius1 = detect_contour(Gdisque)
+    
+    # Dessiner le cercle sur l'image originale
+    cv.circle(img, center1, radius1, (0, 0, 255), 2)
+    cv.circle(img, center1, 2, (0, 0, 255), 3)
+if detect_contour(Pdisque) != None: 
+    center2, radius2 = detect_contour(Pdisque)
+    
+    # Dessiner le cercle sur l'image originale
+    cv.circle(img, center2, radius2, (0, 255, 0), 2)
+    cv.circle(img, center2, 2, (0, 255, 0), 3)
 
-center, radius = find_circle(Gdisque)
-cv.circle(Gdisque, center, radius, (255, 255, 0), 3)
+
+# Afficher l'image avec le cercle
+cv.imshow('Contour detecte', img)
+cv.waitKey(0)
+cv.destroyAllWindows()
 
 # write result to disk
-cv.imwrite("dark_circle_fit.png", Gdisque)
+#cv.imwrite("dark_circle_fit.png", Gdisque)
 
 
+#cv.imshow('Gdisque', Gdisque)
+#cv.waitKey(0)
+#cv.destroyAllWindows()
 
 # Enregistrement des images résultantes
 #cv.imwrite("im_floodfill.png", im_floodfill) # im_floodfill_inv représente le disque intérieur 
@@ -104,6 +111,3 @@ cv.imwrite("dark_circle_fit.png", Gdisque)
 #couleur : la couleur que l’on souhaite attribuer à la zone objet et elle retourne 
 #ret : la valeur du seuil, et th : l’image binaire résultat du seuillage. Afin d’illustrer cela, dans le code suivant, 
 #j’ai seuillé l’image fleur.png préalablement passée en niveau de gris avec 150 comme seuil. 
-
-#hist= cv2.calcHist([img],[i],[256],[0,256])
-#
